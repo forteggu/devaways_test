de4a9f987dc659e81534f280ac967b4b090599a2
Fran <fog1.trabajo@gmail.com>
2021-07-07 09:36:52 +0200

    Rearrange data presentation in general ranking view

--- src/Components/GeneralRanking/GeneralRanking.js
+++ src/Components/GeneralRanking/GeneralRanking.js
@@ -8,10 +8,21 @@ function GeneralRanking() {
       <h1>Clasificación Global</h1>
       <ul>
         {generalRankingList.map((item) => {
+          let rankPosClas;
+          if(item.posicion === 1){
+            rankPosClas=`${cssGR.firstPlace} ${cssGR.rankPosition}`;
+          }else if(item.posicion === 2){
+            rankPosClas=`${cssGR.secondPlace} ${cssGR.rankPosition}`;
+          }else if(item.posicion === 3){
+            rankPosClas=`${cssGR.thirdPlace} ${cssGR.rankPosition}`;
+          }else{
+            rankPosClas=`${cssGR.rankPosition}`;
+          }
           return (
-            <li key={item.datosPiloto.name} className={cssGR.wrapper}>
-              <h2 className={cssGR.rankPosition}>{item.posicion}</h2>
-              <img
+            <li key={item.datosPiloto.name} className={cssGR.wrapper}>  
+              <h3 className={rankPosClas}>{item.posicion}</h3>
+              <div className={cssGR.imgNameContainer}>
+                <img
                 src={item.datosPiloto.photo}
                 alt={"Foto " + item.datosPiloto.name}
                 className={cssGR.foto}
@@ -19,6 +30,7 @@ function GeneralRanking() {
               <span className={cssGR.rankName}>
                 {item.datosPiloto.name} ({item.datosPiloto.team})
               </span>
+              </div>
             </li>
           );
         })}
